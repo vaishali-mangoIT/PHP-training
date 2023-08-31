5197a57622d46bd64b8833fd0c857d693ba7f750
Vaishali Mandloi <vaishali.mandloi@mangoitsolutions.in>
2023-08-31 10:15:28 +0530

    remove unused code

--- PHP_assign5_29/Registration form/submitDetails.php
+++ PHP_assign5_29/Registration form/submitDetails.php
@@ -23,23 +23,12 @@
             echo "succefully connected with database<br>";
         }
 
+        $firstName = $_GET["first_name"];
         $lastName = $_GET["last_name"];
         $email = $_GET["email"];
         $dob = $_GET["dob"];
         $gender = isset($_GET["gender"]) ? $_GET["gender"] : "";
         $password = $_GET["password"];
-
-        // if (empty($dob) || !preg_match('/^\d{4}-\d{2}-\d{2}$/', $dob)) {
-        //     echo "Error: Invalid date of birth format";
-        // } else {
-        //     $sql = "INSERT INTO Registration(first_name, last_name, email, date_of_birth, gender, password) VALUES ('$firstName', '$lastName', '$email', '$dob', '$gender', '$password') ";
-
-        //     if ($conn->query($sql) === TRUE) {
-        //         echo "New record added<br>";
-        //     } else {
-        //         echo "Error(when inserting record) : " . $conn->error;
-        //     }
-        // }
 
         $sql = "INSERT INTO Registration(first_name, last_name, email, date_of_birth, gender, password) VALUES ('$firstName', '$lastName', '$email', '$dob', '$gender', '$password') ";
 
